e26ab61ce89da4ab7a59660fe016f86c3aa99e14
Peter Krevenets <peter.k.nets@gmail.com>
2017-12-12 15:49:53 +0200

    Enable cookies

---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // user already exists
+        done(null, existingUser);
+      } else {
+        // create a new user
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      }
+    }
+  )
+);
